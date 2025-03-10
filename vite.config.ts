8c384b8fb91b57ec12eb9acca495df53f8446b02
p-buddy <parkermalachowsky@gmail.com>
2025-03-10 17:27:47 +0000

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      fileName: 'index',
+      entry: resolve(__dirname, 'src/index.ts'),
+    },
+  },
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
+})
